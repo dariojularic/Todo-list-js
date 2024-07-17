54cd86bf33dcb49b40e0924684dd14d3339772e2
Dario Jularic <dariojularic0@gmail.com>
2024-07-17 22:45:12 +0200

    Create classes

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+class ProjectManager{
+  constructor() {
+    this.projects = [];
+  }
+
+}
+
+class Project {
+  constructor() {
+    this.todos = [];
+    this.isActive = true
+  } 
+}
+
+class Todo {
+  constructor(text, dueDate) {
+    this.text = text;
+    this.id = crypto.randomUUID();
+    this.checked = false;
+    this.time = new Date();
+    this.dueDate = dueDate;
+  }
+}
