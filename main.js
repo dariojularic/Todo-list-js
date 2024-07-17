9a5689ef05912ea06be0ce25353c597a35abd7d8
Dario Jularic <dariojularic0@gmail.com>
2024-07-17 23:23:41 +0200

    Refactor code

--- main.js
+++ main.js
@@ -5,21 +5,54 @@ class ProjectManager{
     this.projects = [];
   }
 
+  findProject(projectId) {
+    return this.projects.find(project => project.id = projectId);
+  }
+
+  deleteProject(projectId) {
+    return this.projects.filter(project => project.id !== projectId)
+  }
 }
 
 class Project {
   constructor() {
+    this.id = crypto.randomUUID();
     this.todos = [];
+    this.isActive = true;
+  }
+
+  findTodo(todoId) {
+    return this.todos.find(todo => todo.id === todoId);
+  }
+
+  activateProject() {
     this.isActive = true
-  } 
+  }
+
+  deactivateProject() {
+    this.isActive = false
+  }
+
+  deleteTodo(todoId) {
+    return this.todos.filter(todo => todo.id !== todoId)
+  }
 }
 
 class Todo {
   constructor(text, dueDate) {
     this.text = text;
+    this.dueDate = dueDate;
     this.id = crypto.randomUUID();
     this.checked = false;
     this.time = new Date();
-    this.dueDate = dueDate;
+  }
+
+  editTodo(text, dueDate) {
+    this.text = text
+    this.dueDate = dueDate
+  }
+
+  toggleChecked() {
+    this.checked = !this.checked
   }
 }
